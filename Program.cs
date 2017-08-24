ef0b6963bd3d08149ac6256406a9ed7db4b5cb36
易墨 <1280160831@qq.com>
2017-08-24 16:05:00 +0800

    update_%1%

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace core_ci_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
